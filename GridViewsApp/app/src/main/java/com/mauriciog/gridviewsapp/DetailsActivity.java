41832a9e0f8c6e0d260867754ad054fe5adc32f9
MauGf <maugarciaflores@gmail.com>
2019-09-07 19:13:29 -0600

    Update DetailsActivity.java

--- GridViewsApp/app/src/main/java/com/mauriciog/gridviewsapp/DetailsActivity.java
+++ GridViewsApp/app/src/main/java/com/mauriciog/gridviewsapp/DetailsActivity.java
@@ -1,12 +1,15 @@
 package com.mauriciog.gridviewsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -30,10 +33,21 @@ public class DetailsActivity extends AppCompatActivity {
         int img;
 
         Bundle b= getIntent().getExtras ();
+        assert b != null;
         detalle=b.getString ("descripcion");
         img=b.getInt ("imagen");
 
         descripcion.setText (detalle);
         imagen.setImageResource (img);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId () == android.R.id.home) {
+            finish ();
+        } else {
+            Toast.makeText (getApplicationContext (), item.getTitle (), Toast.LENGTH_SHORT).show ();
+        }
+        return super.onOptionsItemSelected (item);
     }
 }
